4336cc46d78ac118a4b82a43f8f6fce4becb6e2b
Farzana Zainal <tarzanazainal@gmail.com>
2022-05-16 23:30:38 +0100

    sliders work

--- Assets/Scripts/Unity/ManageGridSlider.cs
+++ Assets/Scripts/Unity/ManageGridSlider.cs
@@ -9,38 +9,39 @@ public class ManageGridSlider : MonoBehaviour
     public Sprite sprite;
     // public float x_boundary;
     // public float y_boundary;
+    [System.Serializable]
     public struct GridParameters{
         public string type;
         public int panel;    // left = -1, right = +1
-        public int size;
-        public int spacing;
+        public float size;
+        public float spacing;
 
-        public GridParameters(string t, int p, int si, int sp){
+        public GridParameters(string t, int p, float si, float sp){
             type = t;
             panel = p;
             size = si;
             spacing = sp;
         }
     }
+    [System.Serializable]
+    public struct SliderValues{
+        public float size_left;
+        public float size_right;
+        public float spacing_left;
+        public float spacing_right;
 
-    struct SliderValues{
-        public int size_left;
-        public int size_right;
-        public int spacing_left;
-        public int spacing_right;
-
-        public SliderValues(int sil, int sir, int spl, int spr){
+        public SliderValues(float sil, float sir, float spl, float spr){
             size_left = sil;
             size_right = sir;
             spacing_left = spl;
             spacing_right = spr;
         }
     }
-
-    private GridParameters leftGrid;
-    private GridParameters rightGrid;
-    private SliderValues slider;
+    public GridParameters leftGrid;
+    public GridParameters rightGrid;
+    public SliderValues slider;
     // public int trial = 0;
+    // private GameObject newGrid;
 
     void Start()
     {
@@ -50,8 +51,16 @@ public class ManageGridSlider : MonoBehaviour
         rows = vertical * (int)(2.2f/sizeFactor);
         // grid = new float[columns, rows];
 
-        leftGrid = new GridParameters("A",-1,0,0);
-        rightGrid = new GridParameters("A",1,0,0);
+        Debug.Log(columns);
+        Debug.Log(rows);
+
+        leftGrid = new GridParameters("A",-1,1,0);
+        rightGrid = new GridParameters("A",1,1,0);
+
+        slider = new SliderValues(1,1,0,0);
+
+        // updateGrid();
+        // newGrid = new GameObject("grid");
     }
 
     // Update is called once per frame
@@ -60,7 +69,7 @@ public class ManageGridSlider : MonoBehaviour
         updateGridParameters();
     }
 
-    private void SpawnSingleTile(float x, float y, int size, GameObject parentGrid){
+    private void SpawnSingleTile(float x, float y, float size, GameObject parentGrid){
         GameObject g = new GameObject("x: "+x+" y: "+y);
         g.AddComponent<BoxCollider2D>();
         var s = g.AddComponent<SpriteRenderer>();
@@ -68,48 +77,54 @@ public class ManageGridSlider : MonoBehaviour
 
         g.transform.position = new Vector3(x,y,0);
         g.transform.parent = parentGrid.transform;
-        g.transform.localScale += new Vector3(size, size, 0);
+        g.transform.localScale = new Vector3(size, size, 0);
         
     }
 
-    private void GenerateGrid(GridParameters parameters){
-        // delete old grid
-        GameObject curGrid = GameObject.Find("grid");
-        if(curGrid != null){
-            Destroy(curGrid);
-        }
+    private void GenerateGrid(GridParameters parameters, GameObject grid){
+        float total_spacing = parameters.size + parameters.spacing;
 
-        // make new grid
-        GameObject newGrid = new GameObject("grid");
-        newGrid.transform.position = this.gameObject.transform.position;
-
-        int total_spacing = parameters.size + (parameters.spacing * parameters.panel);
-
-        for (int j = (int)this.gameObject.transform.position.y; j < rows; j+= total_spacing){
-            for (int i = (int)this.gameObject.transform.position.x; i < columns; i+=total_spacing){
-                SpawnSingleTile(i,j, parameters.size, newGrid);
+        for (int j = 0; j < rows; j++){
+            for (int i = 0; i < columns; i++){
+                float pos_x = this.gameObject.transform.position.x + (i * total_spacing  * parameters.panel);
+                float pos_y = this.gameObject.transform.position.y + (j * total_spacing);
+                SpawnSingleTile(pos_x, pos_y, parameters.size, grid);
             }
         }
     }
 
     public void updateGrid(){
-        GenerateGrid(leftGrid);
-        GenerateGrid(rightGrid);
+        // delete old grid
+        GameObject curGrid_left = GameObject.Find("gridLeft");
+        GameObject curGrid_right = GameObject.Find("gridRight");
+        Destroy(curGrid_left);
+        Destroy(curGrid_right);
+
+        // make new grid
+        GameObject newGrid_left = new GameObject("gridLeft");
+        GameObject newGrid_right = new GameObject("gridRight");
+        newGrid_left.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, -10);
+        newGrid_right.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 10);
+
+        GenerateGrid(leftGrid, newGrid_left);
+        GenerateGrid(rightGrid, newGrid_right);
+
+        // SpawnSingleTile(0, 0, leftGrid.size, newGrid);
     }
 
-    public void AdjustSizeLeft(int newSize){
+    public void AdjustSizeLeft(float newSize){
         slider.size_left = newSize;
     }
 
-    public void AdjustSpacingLeft(int newSpacing){
+    public void AdjustSpacingLeft(float newSpacing){
         slider.spacing_left = newSpacing;
     }
 
-    public void AdjustSizeRight(int newSize){
+    public void AdjustSizeRight(float newSize){
         slider.size_right = newSize;
     }
 
-    public void AdjustSpacingRight(int newSpacing){
+    public void AdjustSpacingRight(float newSpacing){
         slider.spacing_right = newSpacing;
     }
 
